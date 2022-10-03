43c20f14b218a8ef4588fc690b6109d65f01c3f9
= <andre.silva@hubdoctor.com.br>
2022-10-03 20:29:17 -0300

    Adicionar arquivos de projeto.

---
+++ ProjApiAula20221003/Data/ProjApiAula20221003Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjApiAula20221003.Models;
+
+namespace ProjApiAula20221003.Data
+{
+    public class ProjApiAula20221003Context : DbContext
+    {
+        public ProjApiAula20221003Context (DbContextOptions<ProjApiAula20221003Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjApiAula20221003.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ ProjApiAula20221003/Models/Cliente.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjApiAula20221003.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
